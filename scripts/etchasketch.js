36717475fa3d27493cb08b1e0a01ba266a2082b0
Dylan Hull <dbh133@hotmail.com>
2022-09-12 19:05:03 -0400

    base code written.  Initial grid created.

--- scripts/etchasketch.js
+++ scripts/etchasketch.js
@@ -1,25 +1,40 @@
 const body = document.querySelector('.container');
 const btn = document.querySelector('button');
-for (let i = 0; i < 16; i++) {
+let gridSize = 16;
+const container = document.querySelector('.container');
+
+for (let i = 0; i < gridSize ** 2; i++) {
 	const div = document.createElement('div');
 	div.style.border = 'solid .5px';
-	div.style.width = '100px';
-	div.style.height = '100px';
-	div.classList.add(`div${i}`);
 	body.appendChild(div);
+	container.style.gridTemplateColumns = `repeat(${gridSize}, 10px)`;
+	container.style.gridTemplateRows = `repeat(${gridSize}, 10px)`;
 }
 
 const div = document.querySelectorAll('div');
 
+btn.addEventListener('click', () => {
+	gridSize = parseInt(prompt('How many squares per side would you like?'));
+	div.forEach((divSquares) => {
+		if (divSquares.className !== 'container') {
+			divSquares.style.backgroundColor = `white `;
+		}
+	});
+
+	for (let i = 0; i < gridSize ** 2; i++) {
+		const div = document.createElement('div');
+		div.style.border = 'solid .5px';
+		div.classList.add(`div${i}`);
+		body.appendChild(div);
+		container.style.gridTemplateColumns = `repeat(${gridSize}, 10px)`;
+		container.style.gridTemplateRows = `repeat(${gridSize}, 10px)`;
+	}
+});
+
 div.forEach((divSquares) => {
 	if (divSquares.className !== 'container') {
 		divSquares.addEventListener('mouseover', () => {
-			let r = Math.floor(Math.random() * 255 + 1);
-			let g = Math.floor(Math.random() * 255 + 1);
-			let b = Math.floor(Math.random() * 255 + 1);
-			let a = Math.floor(Math.random() * 100 + 1);
-
-			divSquares.style.backgroundColor = `rgb(${r}, ${g}, ${b}, ${a}) `;
+			divSquares.style.backgroundColor = `gray `;
 		});
 	}
 });
